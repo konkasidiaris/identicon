610dc4ccdee6722ea781c15fdf053e562de83d5b
Konstantinos <kkasidiaris@gmail.com>
2021-01-14 22:55:50 +0200

    init commit

---
+++ lib/identicon.ex
@@ -0,0 +1,19 @@
+defmodule Identicon do
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
+
+  def hash_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [red, green, blue | _tail]} = image) do
+    %Identicon.Image{image | color: {red, green, blue}}
+  end
+end
